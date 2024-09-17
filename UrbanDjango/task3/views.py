47057b7a6214168363be65fd9950d8f6125061aa
ddutov <165052103+ddutov@users.noreply.github.com>
2024-09-17 19:48:14 +0300

    views.py

--- UrbanDjango/task3/views.py
+++ UrbanDjango/task3/views.py
@@ -3,8 +3,20 @@ from django.views.generic import TemplateView
 
 
 # Create your views here.
+# def games_list(request):
+#     return render(request, 'games.html')
+
 def games_list(request):
-    return render(request, 'games.html')
+    game_1 = 'Atomic Heart '
+    game_2 = 'Cyberpunk 2077 '
+    game_3 = 'PayDay 2 '
+    context = {
+        'game_1': game_1,
+        'game_2': game_2,
+        'game_3': game_3,
+    }
+    return render(request, 'games.html', context)
+
 
 
 def cart_view(request):
